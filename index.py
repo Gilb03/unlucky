25c3b6514a0ff509d3dbf3fbd59ce0720707b296
Gilbert King <gilbertking91@gmail.com>
2020-04-30 17:17:11 -0500

    cleanup

--- index.py
+++ index.py
@@ -1,7 +1,48 @@
-for num in range(1,21):
+for num in range(1,19):
     if num % 2 == 0:
         print(f"{num} is even")
     elif num == 4 or num == 13:
+        print(f"{num} is unlucky")
+else:
+        print(f"{num} is odd")
+
+
+for num in range(20,39):
+    if num % 2 == 0:
+        print(f"{num} is even")
+    elif num == 4 or num == 13:
+        print(f"{num} is unlucky")
+else:
+        print(f"{num} is odd")
+
+
+
+for num in range(40,70):
+    if num % 2 == 0:
+        print(f"{num} is even")
+    elif num == 2 or num == 12:
+        print(f"{num} is unlucky")
+else:
+        print(f"{num} is odd")
+
+
+
+
+for num in range(71,100):
+    if num % 2 == 0:
+        print(f"{num} is even")
+    elif num == 6 or num == 14:
+        print(f"{num} is unlucky")
+else:
+        print(f"{num} is odd")
+
+
+
+
+for num in range(100,210):
+    if num % 2 == 0:
+        print(f"{num} is even")
+    elif num == 10 or num == 23:
         print(f"{num} is unlucky")
 else:
         print(f"{num} is odd")
